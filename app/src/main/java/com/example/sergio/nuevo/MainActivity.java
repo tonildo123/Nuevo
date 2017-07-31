18a9ba1c2b7a599769670d80c749584aa8d64236
jemanuelp <jemanuelpachecoo@gmail.com>
2017-07-31 15:11:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sergio/nuevo/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.sergio.nuevo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+        private TextView tv3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tv3 = (TextView)findViewById(R.id.textView3);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager m =  getSupportFragmentManager();
+
+        if (id == R.id.nav_camera) {
+            tv3.setText("");
+            m.beginTransaction().replace(R.id.contenedor, new ProgresarR()).commit();
+        } else if (id == R.id.nav_gallery) {
+            tv3.setText("");
+            m.beginTransaction().replace(R.id.contenedor, new ProgresarC()).commit();
+        } else if (id == R.id.nav_slideshow) {
+            tv3.setText("");
+            m.beginTransaction().replace(R.id.contenedor, new JovenesR()).commit();
+        } else if (id == R.id.nav_manage) {
+            tv3.setText("");
+            m.beginTransaction().replace(R.id.contenedor, new JovenesC()).commit();
+        } else if (id == R.id.nav_share) {
+            tv3.setText("");
+            m.beginTransaction().replace(R.id.contenedor, new Mipyme()).commit();
+        } else if (id == R.id.nav_send) {
+                  System.exit(0);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
