84de1c6b2da24b462894e4322e7dc231eba7d9f5
tonildo123 <tony81191@gmail.com>
2018-02-07 10:13:43 -0300

    array de botones

--- app/src/main/java/com/example/sergio/nuevo/presentacion/vistas/BotonesMenu.java
+++ app/src/main/java/com/example/sergio/nuevo/presentacion/vistas/BotonesMenu.java
@@ -24,7 +24,8 @@ import com.example.sergio.nuevo.presentacion.tabs.TabReqProg;
 public class BotonesMenu extends Fragment {
 
 
-    private Button b1, b2, b3, b4, b5, b6;
+    private Button[] button = new Button[6];  // b1, b2, b3, b4, b5, b6;
+    FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
     public BotonesMenu() {
         // Required empty public constructor
@@ -36,52 +37,45 @@ public class BotonesMenu extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista =  inflater.inflate(R.layout.botonesmenus, container, false);
-        b1 =  (Button)vista.findViewById(R.id.bMipyme);
-        b2 =  (Button)vista.findViewById(R.id.bjovenes );
-        b3 =  (Button)vista.findViewById(R.id.bConsulta);
-        b4 =  (Button)vista.findViewById(R.id.bProresar);
-        b5 =  (Button)vista.findViewById(R.id.bNoticias);
-        b6 =  (Button)vista.findViewById(R.id.bMapa);
+        button[0] =  (Button)vista.findViewById(R.id.bMipyme);
+        button[1] =  (Button)vista.findViewById(R.id.bjovenes );
+        button[2] =  (Button)vista.findViewById(R.id.bConsulta);
+        button[3] =  (Button)vista.findViewById(R.id.bProresar);
+        button[4] =  (Button)vista.findViewById(R.id.bNoticias);
+        button[5] =  (Button)vista.findViewById(R.id.bMapa);
 
-        mostrarContenido(b1, b2, b3, b4, b5, b6);
+        mostrarContenido(button);
 
         return vista;
 
     }
 
-    private void mostrarContenido(Button b1, Button b2, Button b3, Button b4, Button b5, Button b6) {
-         b1.setOnClickListener(new View.OnClickListener() {
+    private void mostrarContenido( Button[] button) {
+
+        button[0].setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            transaction.replace(R.id.contenedor, new ModuloMIPyme()).commit();}});
-        b2.setOnClickListener(new View.OnClickListener() {
+                transaction.replace(R.id.contenedor, new ModuloMIPyme()).commit();}});
+        button[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.contenedor, new TabReqJoven()).commit();}});
-        b3.setOnClickListener(new View.OnClickListener() {
+        button[2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.contenedor, new ConsultaLiquidacion()).commit(); }});
-        b4.setOnClickListener(new View.OnClickListener() {
+        button[3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.contenedor, new TabReqProg()).commit(); }});
-        b5.setOnClickListener(new View.OnClickListener() {
+        button[4].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.contenedor, new VistaNoticias()).commit();}});
-        b6.setOnClickListener(new View.OnClickListener() {
+        button[5].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.contenedor, new Mapas()).commit();}});
-
-    }
+                transaction.replace(R.id.contenedor, new Mapas()).commit();}});    }
 
 
 }
